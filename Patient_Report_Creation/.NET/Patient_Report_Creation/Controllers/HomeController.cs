759b81b709b1856dd1c0e3f4248680c20ce4eef1
Kathiresan4347 <159137198+Kathiresan4347@users.noreply.github.com>
2026-05-26 19:58:06 +0530

    1012741: Changes added

--- Patient_Report_Creation/.NET/Patient_Report_Creation/Controllers/HomeController.cs
+++ Patient_Report_Creation/.NET/Patient_Report_Creation/Controllers/HomeController.cs
@@ -71,7 +71,7 @@ namespace Patient_Report_Creation.Controllers
             if (file != null && file.Length > 0)
             {
                 string extension = Path.GetExtension(file.FileName).ToLower();
-                string[] supportedExtensions = { ".doc", ".docx", ".dot", ".dotx", ".dotm", ".docm", ".xml", ".rtf", ".html" };
+                string[] supportedExtensions = { ".doc", ".docx", ".dot", ".dotx", ".dotm", ".docm", ".xml", ".rtf" };
                 // Validate the file extension
                 if (supportedExtensions.Contains(extension))
                 {
